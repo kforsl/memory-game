216d2992e43688f0431e3b8ec538aaa00b385adf
kforsl <forslund95@live.com>
2024-02-07 11:26:52 +0100

    generateCardOrder()

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+function generateCardOrder() {
+    const deck = [];
+    const suffledDeck = [];
+
+    cards.forEach(card => {
+        for (let i = 0; i < 2; i++) {
+            deck.push(card)
+        }
+    });
+
+    for (let j = deck.length; j > 0; j--) {
+        suffledDeck.push(...deck.splice(Math.floor(Math.random() * deck.length), 1))
+    }
+    return suffledDeck
+}
