5691464f4ef1af667e893a36c1f2114c03936a24
kforsl <forslund95@live.com>
2024-02-07 11:34:08 +0100

    function setGameArea

---
+++ scripts/cards.js
@@ -0,0 +1,38 @@
+let cards = [
+    {
+        cardImg: `./assets/coiled-snake.svg`,
+        cardName: `Snake`
+    },
+    {
+        cardImg: `./assets/hare,-rabbit-or-bunny-standing.svg`,
+        cardName: `Rabbit`
+    },
+    {
+        cardImg: `./assets/hedgehog.svg`,
+        cardName: `Hedgehog`
+    },
+    {
+        cardImg: `./assets/side-facing-squirrel.svg`,
+        cardName: `Squirrel`
+    },
+    {
+        cardImg: `./assets/side-facing-turtle.svg`,
+        cardName: `Turtle`
+    },
+    {
+        cardImg: `./assets/sleeping-cat.svg`,
+        cardName: `Cat`
+    },
+    {
+        cardImg: `./assets/sleeping-dog-lying-down.svg`,
+        cardName: `Dog`
+    },
+    {
+        cardImg: `./assets/small-bird-looking-up.svg`,
+        cardName: `Bird`
+    },
+    {
+        cardImg: `./assets/head-of-a-unicorn.svg`,
+        cardName: `Unicorn`
+    }
+];
